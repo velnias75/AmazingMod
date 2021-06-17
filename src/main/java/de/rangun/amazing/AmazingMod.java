8ab8d7e5c36396ed1108b6c1bacb01f1422a8f67
Heiko Schaefer <heiko@rangun.de>
2021-06-17 10:00:12 +0200

    initial commit

---
+++ src/main/java/de/rangun/amazing/AmazingMod.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of AmazingMod.
+ *
+ * AmazingMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * AmazingMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AmazingMod.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.rangun.amazing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class AmazingMod implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
